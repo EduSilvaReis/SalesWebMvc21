a2bc9eb43acf920c448eb03d6635b540a6ac25cb
Eduardo Silva dos Reis <eduardo2001reis@gmail.com>
2022-02-28 10:27:07 -0300

    Returning custom error page

--- SalesWebMvc21/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMvc21/Models/ViewModels/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace SalesWebMvc21.Models.ViewModels
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
