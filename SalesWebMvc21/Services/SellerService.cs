2a1efd6c88995c6f1a98b9d89787bde1b9570586
Eduardo Silva dos Reis <eduardo2001reis@gmail.com>
2022-02-28 09:32:37 -0300

    Update seller and custom service exception

--- SalesWebMvc21/Services/SellerService.cs
+++ SalesWebMvc21/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using SalesWebMvc21.Models;
 using SalesWebMvc21.Data;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMvc21.Services.Exceptions;
 
 namespace SalesWebMvc21.Services
 {
@@ -38,5 +39,24 @@ namespace SalesWebMvc21.Services
             _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
+
+        public void Update(Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+            
+        }
+
     }
 }
